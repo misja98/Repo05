374be34fbc034f07b431dde8e8fff1017d1222ab
misja98 <mirko.djukic@icloud.com>
2021-09-17 14:43:45 +0200

    Second example

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this
+    var myLink = '<a href="http://www.example.com" target ="_blank">Link</a>'
+    
+    //Only change code above this
+    
+    return myLink;
+    };
+    
+    console.log(main())
+    module.exports = main;
+    
