a3ceef5ece960e966494b41e3a2da21d9d2129c4
misja98 <mirko.djukic@icloud.com>
2021-09-17 20:43:51 +0200

    Add third and update second

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this
+    var myString =
+   "FirstLine\n\t\\SecondLine\nThirdLine";
+    //Only change code above this
+    
+    return myString;
+    };
+    
+    console.log(main())
+    module.exports = main;
+    
