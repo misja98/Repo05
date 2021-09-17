b6929c5ed048fb06249c0f659c3d22cfff3e6400
misja98 <mirko.djukic@icloud.com>
2021-09-17 14:13:13 +0200

    First example

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+//Only change code below this
+var myFirstString = "I think therefore I \"am\" -- sentient."
+var mySecondString = "No, you only \"think\", so you aren't."
+//Only change code above this
+
+return {
+    myFirstString,
+    mySecondString
+};
+}
+console.log(main())
+module.exports = main;
