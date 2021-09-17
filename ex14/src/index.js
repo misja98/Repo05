540ed85bd8bd486194202b4d9d6a962ccdeaaa99
misja98 <mirko.djukic@icloud.com>
2021-09-17 22:33:34 +0200

    Fourteenth

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //Only change code below this
+var myNoun = "dog";
+var myVerb = "ran";
+var myAdjective = "big";
+var myAdverb = "quickly";
+var wordBlanks = "A" + " " + myAdjective + " "+ "and" + " " + "angry"+ " " + myNoun + " " + "today" + " " + myVerb + " " + "really" + " " + myAdverb + ".";
+    //Only change code above this
+    
+    return wordBlanks;
+    }
+    console.log(main());
+    module.exports = main;
