a5f560c7a48f0518efeb498a7a5e96666a9a912a
misja98 <mirko.djukic@icloud.com>
2021-09-17 20:54:13 +0200

    Fourth example

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    //Only change code below this
+    var myString ="This is the first string. " + "This is the second string."
+    //Only change code above this
+    
+    return myString;
+    }
+    console.log(main())
+    module.exports = main;
