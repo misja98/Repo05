a3953185ba0b30a6887a2e063afeebfb9cd4ac03
misja98 <mirko.djukic@icloud.com>
2021-09-17 22:08:56 +0200

    tenth

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //Only change code below this
+var myString = "Jello World"
+myString = "Hello World"
+    //Only change code above this
+    
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
