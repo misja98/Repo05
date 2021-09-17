11e539b3b1e99c1f825657f644f21ad44f3aa6cd
misja98 <mirko.djukic@icloud.com>
2021-09-17 21:59:34 +0200

    Eighth

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //Only change code below this
+var lastName = "Doe";
+var lastNameLength = lastName.length;
+    //Only change code above this
+    
+    return lastNameLength;
+    }
+    console.log(main());
+    module.exports = main;
