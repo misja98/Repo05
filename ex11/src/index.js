7627df4f98c956d1ae40b2ef004f1d258ef0304e
misja98 <mirko.djukic@icloud.com>
2021-09-17 22:15:07 +0200

    eleventh

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,10 +1,10 @@
-function main(){
+function main() {
     //Only change code below this
-var myString = "Jello World";
-myString = "Hello World";
+var lastName = "Tesla";
+var thirdLetterOfLastName = lastName[2];
     //Only change code above this
     
-    return myString;
+    return thirdLetterOfLastName;
     }
     console.log(main());
     module.exports = main;
