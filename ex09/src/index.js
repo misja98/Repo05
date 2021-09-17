fcdb61f241a1a3dc585fe8b779f2c5c722716108
misja98 <mirko.djukic@icloud.com>
2021-09-17 22:06:06 +0200

    ninth

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this
+var lastName = "Doe";
+var firstLetterOfLastName = " ";
+var firstLetterOfLastName = lastName[0];
+    //Only change code above this
+    
+    return firstLetterOfLastName;
+    }
+    console.log(main());
+    module.exports = main;
