be82aa1c8381da1e27f353dcc52fd738010a71d0
misja98 <mirko.djukic@icloud.com>
2021-09-17 22:21:16 +0200

    thirteenth

---
+++ ex13/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    //Only change code below this
+var lastName = "Bezos";
+var thirdToLastLetterOfLastName = lastName[lastName.length - 3];
+    //Only change code above this
+    
+    return thirdToLastLetterOfLastName;
+    }
+    console.log(main());
+    module.exports = main;
