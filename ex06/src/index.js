f63008678263d5aa664ceb936c4255776decdcda
misja98 <mirko.djukic@icloud.com>
2021-09-17 21:45:11 +0200

    Update and sixth

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //Only change code below this
+  var myName = "Mirko";
+  var myString = "My name is " + myName + ", how are you?";
+    //Only change code above this
+    
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
