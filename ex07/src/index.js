6cbd7523c9a323b62dcc49e464160e0ecc6b57d1
misja98 <mirko.djukic@icloud.com>
2021-09-17 21:51:54 +0200

    Seventh example

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this
+var someAdjective = "the best!";
+var myString = "Mistral is ";
+myString += someAdjective;
+    //Only change code above this
+    
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
